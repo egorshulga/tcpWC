526332af8ed15027c649ad4f7352051c13e3ea8f
Egor Shulga <scrilan@gmail.com>
2015-03-14 22:34:14 +0300

    parsing server ip finished

--- tcpWC/client.cs
+++ tcpWC/client.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace tcpWC
 {
 	class client
 	{
+		private TcpClient tcpClient = new TcpClient();
+		IPAddress serverIP;
+		private int port = 41414;
+
+		public client(string serverIPtext)
+		{
+			serverIP = TryParseAddress(serverIPtext);
 
 
+		}
+
+		private IPAddress TryParseAddress(string serverIPtext)
+		{
+			try
+			{
+				return IPAddress.Parse(serverIPtext);
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Failed to parse server IP");
+				//throw;
+			}
+		}
 
 	}
 }
