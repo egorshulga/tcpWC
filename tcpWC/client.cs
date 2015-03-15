be74adb6e4a21394a6ad2a2d9bfea0c395c0780c
Egor Shulga <scrilan@gmail.com>
2015-03-15 14:09:55 +0300

    client finished

--- tcpWC/client.cs
+++ tcpWC/client.cs
@@ -1,38 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace tcpWC
 {
-	class client
+	public static class Client
 	{
-		private TcpClient tcpClient = new TcpClient();
-		IPAddress serverIP;
-		private int port = 41414;
+		private static readonly TcpClient tcpClient = new TcpClient();
+		private static IPAddress serverIP;
+		private const int serverPort = 41414;
 
-		public client(string serverIPtext)
-		{
-			serverIP = TryParseAddress(serverIPtext);
+		private const int wordsCountOnError = 0;
 
-
-		}
-
-		private IPAddress TryParseAddress(string serverIPtext)
+		public static int GetWordsCount(string serverIPtext, string text)
 		{
 			try
 			{
-				return IPAddress.Parse(serverIPtext);
+
+				ParseServerIP(serverIPtext);
+
+				ConnectToServer();
+
+				SendTextToServer(text);
+
+				int wordsCount = ReceiveWordsCount();
+
+				CloseConnectionToServer();
+
+				return wordsCount;
 			}
-			catch (Exception)
+			catch (Exception e)
 			{
-				MessageBox.Show("Failed to parse server IP");
-				//throw;
+				MessageBox.Show(e.Message);
+				return wordsCountOnError;
 			}
+		}
+
+
+		private static void ParseServerIP(string serverIPtext)
+		{
+			serverIP = IPAddress.Parse(serverIPtext);
+		}
+
+
+		private static void ConnectToServer()
+		{
+			tcpClient.Connect(serverIP, serverPort);
+		}
+
+
+		private static void SendTextToServer(string textToSend)
+		{
+			byte[] sendingBuffer = Encoding.Unicode.GetBytes(textToSend);
+			NetworkStream sendingStream = tcpClient.GetStream();
+
+			sendingStream.Write(sendingBuffer, 0, sendingBuffer.Length);
+		}
+
+
+		private static int ReceiveWordsCount()
+		{
+			NetworkStream receivingStream = tcpClient.GetStream();
+			byte[] receivingBuffer = new byte[sizeof (int)];
+
+			receivingStream.Read(receivingBuffer, 0, receivingBuffer.Length);
+
+			return ConvertByteArrayToInt(receivingBuffer);
+		}
+
+
+		private static int ConvertByteArrayToInt(byte[] array)
+		{
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(array);
+			}
+			return BitConverter.ToInt32(array, 0);
+		}
+
+
+		static private void CloseConnectionToServer()
+		{
+			tcpClient.Close();
 		}
 
 	}
